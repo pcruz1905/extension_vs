a2eb0097ea621059c7f11b0fdcf9eb17c156718a
Pruzzz <1240589@isep.ipp.pt>
2025-11-12 16:38:52 +0000

    feat: add initial setup for sellhub-liquid workspace with language support and VSCode configurations

---
+++ sellhub-liquid/packages/language/src/index.ts
@@ -0,0 +1,5 @@
+export * from './sellhub-liquid-module.js';
+export * from './sellhub-liquid-validator.js';
+export * from './generated/ast.js';
+export * from './generated/grammar.js';
+export * from './generated/module.js';
